d3da9427de9d086abbf382e2075592b3c35a1cc4
Alejandro <soaresalejandro@outlook.com>
2022-01-12 16:41:57 -0300

    add: tasks file

---
+++ api/apis/tasks.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+# Django Rest Framework
+from rest_framework.decorators import api_view
+
+# Own
+from .models import Category, Api
+from .serializers import PopulateSerializer
+
+# Thrid parties
+import requests
+from celery import shared_task
+
+URL_API = 'https://api.publicapis.org/entries'
+
+
+@shared_task(bind=True)
+def populate_db():
+
+    res = requests.get(URL_API)
+
+    if res.status_code == 200:
+        content = res.json()
+
+        entries = content['entries']
+
+        for row in entries:
+
+            serializer = PopulateSerializer(data=row)
+
+            if serializer.is_valid():
+                serializer.save()
